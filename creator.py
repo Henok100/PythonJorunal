9874eabec8df5be244bd1bc30ec9a69879bd4ad4
Henok100 <henokgashaw15@gmail.com>
2024-04-02 12:27:26 +0300

    Fi

--- creator.py
+++ creator.py
@@ -37,7 +37,7 @@ def init_df():
     # Set the filename to the path of the shapefile or CSV file containing the obstacle data
     # You can either uncomment the getFileName() function call and implement it to dynamically retrieve the filename,
     # or manually specify the filename as a string
-    filename = "Barcelona.csv"
+    filename = "Madrid.csv"
 
     # Print a message to indicate that the shapefile is being read
     print("Reading shapeFile")
@@ -126,7 +126,7 @@ def make_wall(wall, height):
 
     # Print or use the calculated values for further processing
     # print_wall(posx, posy, angle, length, 5, height)
-    print_wall(posx,posy,angle,length,randint(10, 15),height)
+    print_wall(posx,posy,angle,length,randint(10, 15),1)
 
 
 
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     df = init_df()
     
     # Set up the coordinate converter using the UTM zone 30 and WGS84 ellipsoid
-    converter = Proj(proj='utm', zone=31, ellps='WGS84', preserve_units=False)
+    converter = Proj(proj='utm', zone=30, ellps='WGS84', preserve_units=False)
     
     # Define the center coordinates of the area of interest
 
@@ -165,15 +165,15 @@ if __name__ == "__main__":
     # centerY = 4372199.89
 
 	# Madrid
-    # centerX = 441440.71
-    # centerY = 4475195.78
+    centerX = 441440.71
+    centerY = 4475195.78
 
     # Barcelona
-    centerX = 431273.65
-    centerY = 4583915.38
+    # centerX = 431273.65
+    # centerY = 4583915.38
 
     # Open the XML file for writing
-    f = open("Barcelona.xml", "w")
+    f = open("Mardid_orie.xml", "w")
     
     # Write the XML file header
     print_header(f)
